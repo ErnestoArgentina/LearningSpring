b05c518061a83047f0f3f09915e963d57982a48a
Ernesto Argentina Cumbe <41967811+ErnestoArgentina@users.noreply.github.com>
2022-02-07 15:27:39 +0200

    Update ProductRepository.java

--- src/main/java/com/erman/crud/crud/repositories/ProductRepository.java
+++ src/main/java/com/erman/crud/crud/repositories/ProductRepository.java
@@ -3,6 +3,9 @@ package com.erman.crud.crud.repositories;
 import com.erman.crud.crud.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    Optional<Product> findByName(String name);
 }
