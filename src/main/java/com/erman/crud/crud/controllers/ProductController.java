508e4175afaae796ebf5b9e75518ad875b9dca1e
Ernesto Argentina Cumbe <41967811+ErnestoArgentina@users.noreply.github.com>
2022-02-07 15:29:27 +0200

    Update ProductController.java

--- src/main/java/com/erman/crud/crud/controllers/ProductController.java
+++ src/main/java/com/erman/crud/crud/controllers/ProductController.java
@@ -1,12 +1,20 @@
 package com.erman.crud.crud.controllers;
 
+import com.erman.crud.crud.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductService service;
+
     @GetMapping("/")
     public String hello() {
         return "Hello word";
     }
+
+    public
+
 }
